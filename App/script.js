6c3bf58436afa80fefb6d2e1b515244f8f51c134
Unseen <vaibhav.abhilas@gmail.com>
2024-11-17 08:05:38 +0000

    Added files

---
+++ App/script.js
@@ -0,0 +1,11 @@
+function readAction(){
+    const act = document.getElementById("action").value;
+    switch (act){
+        case act.includes("add"):
+            pass
+        case act.includes("del"):
+            pass
+        default:
+            
+    }
+}
